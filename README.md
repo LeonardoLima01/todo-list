5637a7e302716360d4d56dc8b0f386cc2db53f28
socry <leonardosbo02@gmail.com>
2023-05-10 14:47:15 -0300

    Fix links

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # todo-list
 
-[Live demo here](https://socryy.github.io/todo-list/)
+[Live demo here](https://LeonardoLima01.github.io/todo-list/)
 
 Todo list app created with HTML, CSS and JS.
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  }
+  plugins: [new NodePolyfillPlugin()],
+  resolve: {
+    fallback: {
+      fs: false,
+      worker_threads: false,
+      module: false,
+      esbuild: false,
+    },
+  },
+};
